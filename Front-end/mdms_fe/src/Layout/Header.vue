1ac57356b3bdb31746070872149932e7e6d6282e
Recyclingbottle <hoholeo382@gmail.com>
2023-11-03 13:54:27 +0900

    헤더 레이아웃 추가

---
+++ Front-end/mdms_fe/src/Layout/Header.vue
@@ -0,0 +1,70 @@
+<template>
+    <header>
+        <!-- 로고 (여기서는 간단한 텍스트로 표현) -->
+        <div class="logo">로고</div>
+
+        <!-- 홈페이지 제목 -->
+        <div class="title">MDMS</div>
+
+        <!-- Navbar -->
+        <!-- isLoggedIn 변수는 사용자의 로그인 상태 -->
+        <nav v-if="isLoggedIn">
+            <ul>
+                <li>{{ username }}</li> <!-- 사용자 이름 -->
+                <li>회원 정보</li>
+                <li>이력 조회</li>
+                <li>로그아웃</li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'PageHeader',
+    data() {
+        return {
+            isLoggedIn: true,  // 사용자의 로그인 상태를 나타내는 변수
+            username: '테스트이름' // 로그인한 사용자의 이름
+        };
+    },
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    align-items: center;
+    padding: 1rem 2rem;
+    background-color: #333;
+    color: white;
+    border-bottom: 1px solid black;
+    /* 하단 경계선 추가 */
+}
+
+.logo {
+    flex: 1;
+    margin-right: 1rem;
+    padding: 0.5rem;
+}
+
+.title {
+    flex: 2;
+    margin-right: 1rem;
+    padding: 0.5rem;
+}
+
+nav {
+    flex: 3;
+    margin-right: 1rem;
+    padding: 0.5rem;
+}
+
+nav ul {
+    display: flex;
+    gap: 1rem;
+    list-style-type: none;
+    /* 리스트 점 제거 */
+    padding-left: 0;
+    /* 리스트 패딩 제거 */
+}</style>
