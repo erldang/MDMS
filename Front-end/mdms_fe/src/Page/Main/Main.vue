a4af678573d5fa818c53561d792185be57e1e6d5
Recyclingbottle <hoholeo382@gmail.com>
2023-11-03 13:54:50 +0900

    초기 메인 페이지

---
+++ Front-end/mdms_fe/src/Page/Main/Main.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>메인 페이지</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'MainPage',
+    components: {
+    }
+}
+</script>
+
+<style scoped></style>
