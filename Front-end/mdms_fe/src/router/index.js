0c3fd38d0906fc185e59c85204cd75c2e130f4fd
Recyclingbottle <hoholeo382@gmail.com>
2023-11-06 09:45:07 +0900

    아이디(이메일)/비밀번호 찾기 router 추가

--- Front-end/mdms_fe/src/router/index.js
+++ Front-end/mdms_fe/src/router/index.js
@@ -1,14 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import LoginPage from '@/Page/Login/Login.vue'
-import RegisterPage from '@/Page/Register/Register.vue'
-import MainPage from '@/Page/Main/Main.vue'  
+import { createRouter, createWebHistory } from 'vue-router';
+import LoginPage from '@/Page/Login/Login.vue';
+import RegisterPage from '@/Page/Register/Register.vue';
+import MainPage from '@/Page/Main/Main.vue';
+import ForgotPassword from '@/Page/ForgotPassword/ForgotPassword.vue';
 import store from '@/store'; // Vuex store 임포트
 
 const routes = [
-    {
-    path: '/',   
+  {
+    path: '/',
     name: 'Main',
-    component: MainPage  
+    component: MainPage
   },
   {
     path: '/login',
@@ -19,19 +20,29 @@ const routes = [
     path: '/register',
     name: 'Register',
     component: RegisterPage
+  },
+  {
+    path: '/forgot-password', // 비밀번호 찾기 경로 추가
+    name: 'ForgotPassword',
+    component: ForgotPassword
   }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
 router.beforeEach((to, from, next) => {
-  if (to.name !== 'Login' && !store.state.isLoggedIn) {
+  // 로그인, 회원가입, 비밀번호 찾기 페이지는 로그인 여부와 상관없이 이동 가능
+  if (to.name === 'Login' || to.name === 'Register' || to.name === 'ForgotPassword') {
+    next();
+  } else if (!store.state.isLoggedIn) {
     next({ name: 'Login' });
   } else {
     next();
   }
 });
+
+
 export default router;
