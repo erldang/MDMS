fae6a693a95ff7f30c70a46760d0fd880faf086b
Recyclingbottle <hoholeo382@gmail.com>
2023-11-06 11:01:54 +0900

    헤더 디자인 변경 & 로그아웃 로직 추가

--- Front-end/mdms_fe/src/Layout/Header.vue
+++ Front-end/mdms_fe/src/Layout/Header.vue
@@ -1,78 +1,118 @@
 <template>
-    <header>
-        <!-- 로고 (여기서는 간단한 텍스트로 표현) -->
-        <div class="logo">로고</div>
-
-        <!-- 홈페이지 제목 -->
-        <div class="title">MDMS</div>
+    <header class="header-container">
+        <!-- Logo & Title Section -->
+        <div class="left-section" @click="navigateToHome">
+            <img src="@/assets/img/test-logo.png" alt="테스트로고" class="logo-img">
+            <div class="title-section">
+                <h1 class="title">MDMS</h1>
+                <p class="subtitle">메타데이터 관리 서비스</p>
+            </div>
+        </div>
 
         <!-- Navbar -->
-        <!-- isLoggedIn 변수는 사용자의 로그인 상태 -->
-        <nav v-if="isLoggedIn">
-            <ul>
-                <li>{{ username }}</li> <!-- 사용자 이름 -->
-                <li @click="navigateToProfile">회원 정보</li>
-                <li>이력 조회</li>
-                <li>로그아웃</li>
-            </ul>
+        <nav v-if="isLoggedIn" class="navbar-section">
+            <span class="username-text">{{ username }}</span>
+            <button class="nav-btn" @click="navigateToProfile">회원 정보</button>
+            <button class="nav-btn">이력 조회</button>
+            <button class="nav-btn logout-btn" @click="logoutAction">로그아웃</button>
         </nav>
     </header>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
     name: 'PageHeader',
     data() {
         return {
-            isLoggedIn: true,  // 사용자의 로그인 상태를 나타내는 변수
-            username: '테스트이름' // 로그인한 사용자의 이름
+            username: '유저이름'
         };
     },
+    computed: {
+        ...mapGetters(['isLoggedIn'])
+    },
     methods: {
-    navigateToProfile() {
-        this.$router.push('/profile');
+        ...mapActions(['login', 'logout']),
+        navigateToProfile() {
+            this.$router.push('/profile');
+        },
+        navigateToHome() {
+            this.$router.push('/').then(() => {
+                this.$router.go(); // 라우터가 재확인하도록 강제
+            });
+        },
+        logoutAction() {
+            this.logout();
+            this.$router.push('/').then(() => {
+                this.$router.go();
+            });
+        }
     }
-}   
 }
 </script>
 
+
+
 <style scoped>
-header {
+.header-container {
     display: flex;
+    justify-content: space-between;
     align-items: center;
     padding: 1rem 2rem;
-    background-color: #333;
-    color: white;
-    border-bottom: 1px solid black;
-
-    padding: 1.25rem;  
-    margin: 0.9375rem; 
+    background-color: #2C3E50;
+    color: #ecf0f1;
+    border-bottom: 2px solid #34495e;
 }
 
-.logo {
-    flex: 1;
-    margin-right: 1rem;
-    padding: 0.5rem;
+.logo-img {
+    width: 40px;
+    height: 40px;
+    margin-right: 0.8rem;
+}
+
+.title-section {
+    display: flex;
+    flex-direction: column;
 }
 
 .title {
-    flex: 2;
-    margin-right: 1rem;
-    padding: 0.5rem;
+    font-size: 1.8rem;
+    margin: 0;
 }
 
-nav {
-    flex: 3;
-    margin-right: 1rem;
-    padding: 0.5rem;
+.subtitle {
+    font-size: 0.85rem;
+    color: #bdc3c7;
 }
 
-nav ul {
-    display: flex;
-    gap: 1rem;
-    list-style-type: none;
-    /* 리스트 점 제거 */
-    padding-left: 0;
-    /* 리스트 패딩 제거 */
-}</style>
+.nav-btn {
+    padding: 0.6rem 1.2rem;
+    margin-left: 0.5rem;
+    background-color: transparent;
+    border: 1px solid #ecf0f1;
+    border-radius: 3px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+    color:#bdc3c7;
+}
+
+.nav-btn:hover {
+    background-color: #34495e;
+}
+
+.logout-btn {
+    border-color: #e74c3c;
+    color: #e74c3c;
+}
+
+.logout-btn:hover {
+    background-color: #e74c3c;
+    color: #ecf0f1;
+}
+
+.username-text {
+    margin-right: 1rem;
+    font-weight: 500;
+}
+</style>
