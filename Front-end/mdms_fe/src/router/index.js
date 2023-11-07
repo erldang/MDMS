7fc88caca60b8ca7c21e03e54e4bb8b9572c870f
Recyclingbottle <hoholeo382@gmail.com>
2023-11-07 16:10:10 +0900

    router, index.js 로그인 상태 확인 코드 수정

--- Front-end/mdms_fe/src/router/index.js
+++ Front-end/mdms_fe/src/router/index.js
@@ -77,13 +77,21 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  // 로그인, 회원가입, 비밀번호 찾기 페이지는 로그인 여부와 상관없이 이동 가능 //  to.name === 'AdminUserRecordPage' || to.name === 'AdminDataMapPage' 는 테스트를 위한 코드입니다 발견하면 지워주시길 바랍니다.
-  if (to.name === 'Login' || to.name === 'Register' || to.name === 'ForgotPassword'|| to.name === 'AdminUserRecordPage' || to.name === 'AdminDataMapPage') {
+  // 로그인, 회원가입, 비밀번호 찾기 페이지는 로그인 여부와 상관없이 이동 가능
+  if (to.name === 'Login' || to.name === 'Register' || to.name === 'ForgotPassword') {
     next();
-  } else if (!store.state.isLoggedIn) {
-    next({ name: 'Login' });
   } else {
-    next();
+    // 토큰을 로컬 스토리지에서 확인합니다.
+    const token = localStorage.getItem('token');
+    
+    // 로그인이 필요한 페이지에 접근할 때 토큰 또는 Vuex 상태를 체크합니다.
+    if (token || store.state.isLoggedIn) {
+      // 토큰이 있거나, Vuex 상태가 로그인 상태라면 다음 페이지로 이동합니다.
+      next();
+    } else {
+      // 토큰이 없거나, Vuex 상태가 로그인 상태가 아니라면 로그인 페이지로 리다이렉트합니다.
+      next({ name: 'Login' });
+    }
   }
 });
 
