702f454ad37308925b1aa01f6dd5ed195496501e
Recyclingbottle <hoholeo382@gmail.com>
2023-11-07 16:11:29 +0900

    회원 정보 확인, 회원 정보 수정, 탈퇴 로직 구현

--- Front-end/mdms_fe/src/Page/Profile/Profile.vue
+++ Front-end/mdms_fe/src/Page/Profile/Profile.vue
@@ -1,71 +1,183 @@
 <template>
     <div class="profile-container">
-        <h1>회원정보</h1>
-        <form>
-            <!-- 이름 정보 -->
-            <div class="input-group">
-                <label for="name" class="kind">이름</label>
-                <input type="text" id="name" placeholder="현재이름 출력됨"/>
-            </div>
-
-            <!-- 이메일 정보 -->
-            <div class="input-group">
-                <label for="email" class="kind">이메일</label>
-                <label for="email">현재이메일 출력됨</label>
-            </div>
-
-            <!-- 비밀번호 수정 -->
-            <div class="input-group">
-                <label for="password" class="kind">비밀번호</label>
-                <button @click.prevent="updatePassword">변경하기</button>
-                <div v-if="showUpdatePassword">
-                    <input type="text" placeholder="비밀번호 입력" />
-                    <input type="text" placeholder="비밀번호 재입력" />
-                    <button @click="checkChangePassword">확인하기</button>
-                </div>
-            </div>
-
-            <!-- 전화번호 정보 -->
-            <div class="input-group">
-                <label for="Phone" class="kind">전화번호</label>
-                <input type="text" id="phone" placeholder="현재 전화번호 출력될 것" />
-            </div>
-
-            <div >
-                <button id="confirm" type="submit" @click="changeProfile">수정하기</button>
-                <button id="withdraw" @click.prevent="withdrawMember">회원탈퇴</button>
-            </div>
-        </form>
+      <h1>회원정보</h1>
+      <form @submit.prevent="changeProfile">
+        <div class="input-group">
+          <label for="name" class="kind">이름</label>
+          <input type="text" id="name" v-model="name" :disabled="!isEditing" />
+        </div>
+  
+        <div class="input-group">
+          <label for="email" class="kind">이메일</label>
+          <label for="email">{{ email }}</label>
+        </div>
+  
+        <div class="input-group">
+          <label for="password" class="kind">비밀번호</label>
+          <button @click.prevent="updatePassword">변경하기</button>
+          <div v-if="showUpdatePassword">
+            <input type="password" v-model="newPassword" placeholder="새로운 비밀번호 입력" />
+            <input type="password" v-model="confirmNewPassword" placeholder="비밀번호 재입력" />
+            <button @click="checkChangePassword">확인하기</button>
+          </div>
+        </div>
+  
+        <div class="input-group">
+          <label for="Phone" class="kind">전화번호</label>
+          <input type="text" id="phone" v-model="phone" :disabled="!isEditing" />
+        </div>
+  
+        <div>
+          <button id="edit" type="button" @click="toggleEditing">{{ isEditing ? '수정 완료' : '수정하기' }}</button>
+          <button id="withdraw" @click="showDeleteConfirmation">회원탈퇴</button>
+        </div>
+      </form>
+  
+      <div v-if="showDeleteDialog" class="delete-dialog">
+        <p>비밀번호를 입력해 주세요.</p>
+        <input type="password" v-model="deletePassword" placeholder="비밀번호 입력" />
+        <div>
+          <button @click="confirmDelete">확인</button>
+          <button @click="cancelDelete">취소</button>
+        </div>
+      </div>
     </div>
-</template>
-
-
-<script>
-import './Profile.css'
-export default {
+  </template>
+  
+  <script>
+  import './Profile.css'
+  export default {
     name: 'ProfilePage',
     data() {
-        return{
-            name: '',
-            email: '',
-            password: '',
-            showUpdatePassword: false,
-            phone: ''
-        }
+      return {
+        name: '',
+        email: '',
+        password: '', 
+        showUpdatePassword: false,
+        phone: '',
+        newPassword: '',
+        confirmNewPassword: '',
+        isEditing: false,
+        errorMessage: '',
+        showDeleteDialog: false,
+        deletePassword: '',
+      }
     },
     methods: {
-        withdrawMember() {
-            // 회원 탈퇴 처리
-        },
-        changeProfile() {
-            // 프로필 변경 처리
-        },
-        checkChangePassword(){
-            // 암호 변경 처리
-        },
-        updatePassword() {
-            this.showUpdatePassword = true;
+      logout() {
+        return new Promise((resolve) => {
+          localStorage.removeItem('token');
+          resolve();
+        });
+      },
+      async fetchUserProfile() {
+        try {
+          const token = localStorage.getItem('token');
+          const response = await fetch(`localhost3001:/user/specific?token=${token}`);
+          const data = await response.json();
+          if (data && data.data) {
+            this.name = data.data.name;
+            this.email = data.data.email;
+            this.phone = data.data.phone;
+          }
+        } catch (error) {
+          console.error('프로필을 가져오는 데 오류가 발생하였습니다:', error);
         }
+      },
+      async changeProfile() {
+        try {
+          const token = localStorage.getItem('token');
+          const dataToUpdate = {
+            "email": this.email,
+            "name": this.name,
+            "phone": this.phone
+            //password 도 필요한 가?
+          };
+  
+          if (this.showUpdatePassword) {
+            dataToUpdate.password = this.newPassword;
+          }
+  
+          const response = await fetch('/user/modify', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify(dataToUpdate)
+          });
+  
+          const data = await response.json();
+          if (data && data.ok === 'ok') {
+            alert('프로필이 성공적으로 업데이트되었습니다.');
+            this.isEditing = false;
+            this.errorMessage = '';
+          } else {
+            this.errorMessage = '프로필 업데이트 중 오류가 발생하였습니다.';
+          }
+        } catch (error) {
+          console.error('프로필 업데이트 중 오류가 발생하였습니다:', error);
+          this.errorMessage = '프로필 업데이트 중 오류가 발생하였습니다.';
+        }
+      },
+      checkChangePassword() {
+        this.changeProfile();
+      },
+      updatePassword() {
+        this.showUpdatePassword = true;
+      },
+      toggleEditing() {
+        this.isEditing = !this.isEditing;
+      },
+      showDeleteConfirmation() {
+        this.showDeleteDialog = true;
+      },
+      confirmDelete() {
+        if (this.deletePassword === this.password) {
+          this.deleteUser().then(() => {
+            this.logout().then(() => {
+              alert('회원 탈퇴가 완료되었습니다.');
+              this.$router.push('/login');
+            }).catch(error => {
+              console.error('Logout failed', error);
+            });
+          });
+        } else {
+          alert('비밀번호가 일치하지 않습니다.');
+        }
+      },
+      cancelDelete() {
+        this.showDeleteDialog = false;
+      },
+      async deleteUser() {
+        try {
+          const token = localStorage.getItem('token');
+          const response = await fetch('localhost:3001/user/delete', {
+            method: 'DELETE',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({
+              email: this.email
+            })
+          });
+          const data = await response.json();
+          if (data && data.ok === 'ok') {
+            alert('회원 탈퇴가 완료되었습니다.');
+            this.router.push('/login');
+          } else {
+            this.errorMessage = '회원 탈퇴 중 오류가 발생하였습니다.';
+          }
+        } catch (error) {
+          console.error('회원 탈퇴 중 오류가 발생하였습니다:', error);
+          this.errorMessage = '회원 탈퇴 중 오류가 발생하였습니다.';
+        }
+      },
+    },
+    created() {
+      this.fetchUserProfile();
     }
-}
-</script>
+  }
+  </script>
+  
