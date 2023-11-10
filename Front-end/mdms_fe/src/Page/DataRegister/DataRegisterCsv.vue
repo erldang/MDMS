98c14c2c2e03f07f184dcd8ac824c257f21e6ea5
Recyclingbottle <hoholeo382@gmail.com>
2023-11-10 14:22:28 +0900

    CSV 등록 후 속성명 변경 할 때 추천어 표시

--- Front-end/mdms_fe/src/Page/DataRegister/DataRegisterCsv.vue
+++ Front-end/mdms_fe/src/Page/DataRegister/DataRegisterCsv.vue
@@ -1,13 +1,19 @@
 <template>
     <div>
-      <h1>CSV 파일 업로드 및 데이터 등록</h1>
+      <h1>CSV 파일 업로드 테스트</h1>
       <input type="file" @change="readCsvFile" />
       <table v-if="tableData.length">
         <thead>
           <tr>
             <th v-for="(header, index) in headers" :key="index">
               {{ header }}
-              <input type="text" v-model="headers[index]" placeholder="속성명 변경" @change="updateHeader(index)" />
+              <input type="text" v-model="headers[index]" placeholder="속성명 변경" @input="suggestHeader($event, index)" />
+              <ul v-if="suggestedHeaders[index] && suggestedHeaders[index].length">
+                <li v-for="(suggestedHeader, keyIndex) in suggestedHeaders[index]" :key="`suggested-${index}-${keyIndex}`" @click="selectHeader(index, suggestedHeader)">
+                {{ suggestedHeader }}
+                </li>
+
+              </ul>
             </th>
           </tr>
         </thead>
@@ -26,14 +32,13 @@
       return {
         headers: [],
         tableData: [],
-        // 더미 데이터로 서버의 응답을 모방합니다.
         serverHeaders: [
           { id: 1, name: '사용자ID' },
           { id: 2, name: '이름' },
           { id: 3, name: '나이' },
           { id: 4, name: '이메일' },
-          // 추가적인 더미 헤더 목록...
         ],
+        suggestedHeaders: [],
       };
     },
     methods: {
@@ -54,14 +59,22 @@
         const tableData = rows.slice(1).map((row) => row.split('|').map((cell) => cell.trim()));
         return { headers, data: tableData };
       },
+      suggestHeader(event, index) {
+        const query = event.target.value;
+        this.suggestedHeaders[index] = this.serverHeaders
+          .filter(header => header.name.toLowerCase().includes(query.toLowerCase()))
+          .map(header => header.name);
+      },
+      selectHeader(index, headerName) {
+        this.headers[index] = headerName;
+        this.suggestedHeaders[index] = [];
+      },
       updateHeader(index) {
-        // 입력된 속성명으로 더미 데이터에서 검색합니다.
         let newHeader = this.serverHeaders.find(header => header.name === this.headers[index]);
         if(newHeader) {
           this.headers.splice(index, 1, newHeader.name);
         } else {
-          // 일치하는 항목이 없을 경우 경고 메시지를 표시합니다.
-          alert('해당 속성명이 목록에 없습니다.');
+            console.log("error");
         }
       }
     }
@@ -69,6 +82,19 @@
   </script>
   
   <style>
-  /* 여기에 CSS 스타일을 추가할 수 있습니다. */
+    /* 스타일을 추가하거나 수정할 수 있습니다 */
+    ul {
+      list-style-type: none;
+      padding: 0;
+    }
+    li {
+      cursor: pointer;
+      background-color: #f0f0f0;
+      padding: 2px 4px;
+      margin-bottom: 2px;
+    }
+    li:hover {
+      background-color: #e0e0e0;
+    }
   </style>
   
