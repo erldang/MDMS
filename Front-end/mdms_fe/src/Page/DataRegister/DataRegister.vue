9945afbf243af768b251280da70ab5d3f6aeeee7
Recyclingbottle <hoholeo382@gmail.com>
2023-11-13 13:53:36 +0900

    데이터 타입이 Numeric 일 때 조건 추가

--- Front-end/mdms_fe/src/Page/DataRegister/DataRegister.vue
+++ Front-end/mdms_fe/src/Page/DataRegister/DataRegister.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="data-register">
     <h2>SQL 테이블 생성</h2>
-    <form @submit.prevent="registerTable">
+    <form @submit.prevent="generateAndRegisterTable">
       <div class="input-group">
         <label for="tableName">테이블 이름 입력:</label>
-        <input type="text" id="tableName" v-model="tableName" />
+        <input type="text" id="tableName" v-model="tableName" @input="validateTableName" />
       </div>
 
       <!-- 컬럼을 동적으로 추가하는 부분 -->
@@ -33,12 +33,13 @@
 
       <!-- SQL DDL 생성 및 등록 버튼 -->
       <div class="actions">
-        <button type="button" @click="generateCreateStatement">SQL DDL 생성</button>
-        <button type="submit">등록하기</button>
+        <button type="button" @click="generateCreateStatement" :disabled="!tableName">SQL DDL 생성</button>
+        <button type="submit" :disabled="!tableName || columns.length === 0">등록하기</button>
       </div>
     </form>
   </div>
 </template>
+
 <script>
 import axios from 'axios';
 
@@ -52,48 +53,56 @@ export default {
     };
   },
   created() {
-    this.fetchServerData();
+    this.fetchServerData(); // 컴포넌트가 생성될 때 서버 데이터를 가져오는 함수를 호출
   },
   methods: {
+    // 서버로부터 데이터를 가져오는 함수
     async fetchServerData() {
       try {
-        const token = localStorage.getItem('token');
+        const token = localStorage.getItem('token'); // 로컬 스토리지에서 토큰 가져오기
         const response = await axios.get('http://localhost:3001/terminology', {
-          headers: { 'Authorization': `Bearer ${token}` }
+          headers: { 'Authorization': `Bearer ${token}` } // 토큰을 헤더에 포함시켜 요청
         });
-        this.serverData = response.data.data;
+        this.serverData = response.data.data; // 응답으로 받은 데이터를 serverData에 저장
       } catch (error) {
-        alert('데이터를 불러오는 중 에러가 발생했습니다.');
-        this.goBack();
+        alert('데이터를 불러오는 중 에러가 발생했습니다.'); // 에러 처리
+        this.goBack(); // 에러 시 이전 페이지로 돌아가기
       }
     },
+    // 이전 페이지로 돌아가는 함수
     goBack() {
       window.history.back();
     },
+    // 새 컬럼을 배열에 추가하는 함수
     addColumn() {
       this.columns.push({ name: '', type: '', standardTerminology: '', description: '' });
     },
+    // 특정 인덱스의 컬럼을 배열에서 제거하는 함수
     removeColumn(index) {
       this.columns.splice(index, 1);
     },
+    // 컬럼 이름으로 서버 데이터를 검색하는 함수
     searchColumnName(index) {
       const searchQuery = this.columns[index].name.toLowerCase();
       this.searchResults[index] = this.serverData.filter(item =>
-        item.standardTerminology.includes(searchQuery)
+        item.standardTerminology.toLowerCase().includes(searchQuery)
       );
     },
+    // 검색 결과를 선택하여 컬럼 정보를 업데이트하는 함수
     selectColumnName(result, index) {
       const dataType = this.formatDataType(result.domain.dataType);
       const dataLength = result.domain.dataLength;
       const decimalPointLength = result.domain.decimalPointLength;
       let typeFormat = dataType;
 
+      // 데이터 타입이 NUMERIC이고 소수점 길이가 '-'가 아니라면 해당 포맷을 적용
       if (dataType === 'NUMERIC' && decimalPointLength !== '-') {
         typeFormat = `NUMERIC(${dataLength}, ${decimalPointLength})`;
       } else if (dataType === 'CHAR' || dataType === 'VARCHAR') {
         typeFormat = `${dataType}(${dataLength})`;
       }
 
+      // columns 배열의 해당 인덱스에 컬럼 정보 업데이트
       this.columns[index] = {
         ...this.columns[index],
         name: result.englishAbbreviation,
@@ -102,25 +111,57 @@ export default {
         description: result.description
       };
 
+      // 검색 결과 초기화
       this.searchResults = {};
     },
+    // 데이터 타입을 대문자로 변환하는 함수
     formatDataType(dataType) {
       return dataType.toUpperCase();
     },
+    // CREATE TABLE 문을 생성하는 함수
     generateCreateStatement() {
-      let createStatement = `CREATE TABLE ${this.tableName} (\n`;
-      createStatement += `No INT AUTO_INCREMENT PRIMARY KEY,\n`;
+      if (!this.tableName.trim() || this.columns.length === 0) {
+        alert('테이블 이름을 입력하거나, 컬럼을 추가해주세요.');
+        return;
+      }
 
+      let createStatement = `CREATE TABLE ${this.tableName} (\n`;
+      createStatement += `No INT AUTO_INCREMENT PRIMARY KEY,\n`; // 테이블 기본 키 설정
+
+      // columns 배열을 순회하면서 각 컬럼의 SQL 문을 생성
       this.columns.forEach((column, index) => {
-        createStatement += `${column.name} ${column.type}`;
-        if (index !== this.columns.length - 1) {
-          createStatement += ',\n';
+        const columnData = this.serverData.find(item => item.englishAbbreviation === column.name);
+        if (columnData) {
+          let dataTypeString = this.getDataTypeString(columnData.domain);
+
+          // 컬럼 정의 추가
+          createStatement += `${column.name} ${dataTypeString}`;
+          if (index < this.columns.length - 1) {
+            createStatement += ',\n'; // 마지막 컬럼이 아니라면 쉼표를 추가
+          }
+        } else {
+          // 일치하는 데이터가 없는 경우 경고 출력
+          console.warn(`No matching data found for column ${column.name}`);
         }
       });
 
-      createStatement += '\n);';
-      console.log(createStatement);
+      createStatement += '\n);'; // SQL 문 마무리
+      console.log(createStatement); // 생성된 SQL 문을 콘솔에 출력
     },
+    // 데이터 타입 문자열을 생성하는 함수
+    getDataTypeString(domain) {
+      let dataTypeString = '';
+      const { dataType, dataLength, decimalPointLength } = domain;
+
+      if (dataType === 'NUMERIC') {
+        dataTypeString = decimalPointLength !== '-' ? `NUMERIC(${dataLength}, ${decimalPointLength})` : `NUMERIC(${dataLength})`;
+      } else {
+        dataTypeString = dataType === 'DATETIME' ? dataType : `${dataType}(${dataLength})`;
+      }
+
+      return dataTypeString;
+    },
+    // 입력 폼을 초기화하는 함수
     resetForm() {
       this.tableName = '';
       this.columns = [];
@@ -131,88 +172,86 @@ export default {
 </script>
 
 
+
 <style scoped>
 .data-register {
-  max-width: 800px; /* 너비를 조정하여 가독성 향상 */
-  margin: 40px auto; /* 상단과 하단의 마진을 추가하여 더 많은 공간을 확보 */
-  padding: 40px; /* 내부 패딩을 늘려 요소들 간의 공간 확보 */
-  background-color: #ffffff; /* 밝은 배경색을 사용하여 깔끔한 외관 제공 */
-  border-radius: 15px; /* 모서리 둥글기를 적용하여 현대적인 느낌 제공 */
-  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.15); /* 그림자를 추가하여 입체감 부여 */
-  border: 1px solid #e0e0e0; /* 경계선을 추가하여 구조적 외관 강화 */
+  max-width: 800px;
+  margin: 40px auto;
+  padding: 40px;
+  background-color: #ffffff;
+  border-radius: 15px;
+  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.15);
+  border: 1px solid #e0e0e0;
 }
 
 .data-register h2 {
   text-align: center;
-  color: #2c3e50; /* 타이틀 색상 변경 */
-  font-size: 24px; /* 타이틀 폰트 크기를 늘려 더 눈에 띄게 함 */
-  margin-bottom: 30px; /* 타이틀과 내용 사이의 여백 증가 */
-  padding-bottom: 10px; /* 타이틀 아래에 패딩 추가 */
-  border-bottom: 2px solid #ecf0f1; /* 타이틀 아래 경계선 추가 */
+  color: #2c3e50;
+  font-size: 24px;
+  margin-bottom: 30px;
+  padding-bottom: 10px;
+  border-bottom: 2px solid #ecf0f1;
 }
 
 .input-group label,
 .column label {
   display: block;
-  margin-bottom: 10px; /* 레이블과 입력 필드 사이의 여백 증가 */
-  font-weight: 600; /* 레이블 폰트 가중치를 높여 중요성 강조 */
-  color: #34495e; /* 레이블 색상 변경 */
+  margin-bottom: 10px;
+  font-weight: 600;
+  color: #34495e;
 }
 
 .input-group input[type="text"],
 .column input[type="text"],
 .column select {
   width: 100%;
-  padding: 12px; /* 입력 필드 패딩 증가 */
-  margin-bottom: 15px; /* 입력 필드 간의 여백 증가 */
-  border: 1px solid #bdc3c7; /* 입력 필드 경계선 색상 변경 */
-  border-radius: 5px; /* 입력 필드 모서리 둥글기 적용 */
-  box-sizing: border-box; /* 박스 크기 결정 방법 변경 */
-  transition: border-color 0.3s; /* 입력 필드 포커스 시 부드러운 색상 변화 효과 */
+  padding: 12px;
+  margin-bottom: 15px;
+  border: 1px solid #bdc3c7;
+  border-radius: 5px;
+  box-sizing: border-box;
+  transition: border-color 0.3s;
 }
 
 .input-group input[type="text"]:focus,
 .column input[type="text"]:focus,
 .column select:focus {
-  border-color: #3498db; /* 입력 필드 포커스 시 경계선 색상 변경 */
-  outline: none; /* 기본 아웃라인 제거 */
+  border-color: #3498db;
+  outline: none;
 }
 
 .button {
-  /* 버튼 스타일 변경 */
-  padding: 12px 20px; /* 버튼 패딩 증가 */
-  font-size: 16px; /* 버튼 폰트 크기 증가 */
-  font-weight: 600; /* 버튼 폰트 가중치 증가 */
-  text-transform: uppercase; /* 버튼 텍스트를 대문자로 변경 */
-  letter-spacing: 1px; /* 버튼 글자 사이 간격 증가 */
-  transition: background-color 0.3s, transform 0.2s; /* 배경색과 변형에 대한 전환 효과 추가 */
+  padding: 12px 20px;
+  font-size: 16px;
+  font-weight: 600;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  transition: background-color 0.3s, transform 0.2s;
 }
 
 .button:hover {
-  background-color: #2980b9; /* 버튼 호버 시 배경색 변경 */
-  transform: translateY(-2px); /* 버튼 호버 시 약간 위로 이동하여 클릭감 향상 */
+  background-color: #2980b9;
+  transform: translateY(-2px);
 }
 
-/* 버튼을 위한 추가적인 호버 효과와 활성화 효과 추가 */
 .button:active {
   transform: translateY(0);
 }
 
 .search-results ul {
-  /* 검색 결과 스타일 변경 */
   padding-left: 0;
-  margin-top: 10px; /* 검색 결과 상단 여백 증가 */
+  margin-top: 10px;
 }
 
 .search-results li {
-  padding: 10px; /* 검색 결과 항목 패딩 증가 */
-  margin-bottom: 8px; /* 검색 결과 항목 간 여백 증가 */
-  background-color: #ecf0f1; /* 검색 결과 배경색 변경 */
-  border-radius: 5px; /* 검색 결과 모서리 둥글기 적용 */
-  transition: background-color 0.3s; /* 검색 결과 호버 시 배경색 변화 효과 */
+  padding: 10px;
+  margin-bottom: 8px;
+  background-color: #ecf0f1;
+  border-radius: 5px;
+  transition: background-color 0.3s;
 }
 
 .search-results li:hover {
-  background-color: #d6dbdf; /* 검색 결과 호버 시 배경색 변경 */
+  background-color: #d6dbdf;
 }
 </style>
