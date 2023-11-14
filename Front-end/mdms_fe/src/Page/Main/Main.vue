69b4be5c84ec49af1fe89a73e93c8136cc2cdc7b
Recyclingbottle <hoholeo382@gmail.com>
2023-11-14 10:07:33 +0900

    메인 페이지 변경

--- Front-end/mdms_fe/src/Page/Main/Main.vue
+++ Front-end/mdms_fe/src/Page/Main/Main.vue
@@ -1,87 +1,100 @@
 <template>
-    <div>
-      <div class="search-filter">
-        <!-- 검색 및 필터링 섹션 -->
-        <input type="date" v-model="startDate" />
-        <input type="date" v-model="endDate" />
-        <select v-model="selectedCategory">
-          <option value="">카테고리</option>
-          <!-- 카테고리 옵션 -->
-        </select>
-        <input type="text" v-model="searchQuery" placeholder="검색어" />
-        <button @click="searchData">검색</button>
-      </div>
-  
-      <h1>데이터 목록</h1>
-      <button @click="addData">등록</button>
-      <div class="button-group">
-        <button @click="navigateToTable">테이블</button>
-        <button @click="navigateToDataMap">데이터맵</button>
-      </div>
-      <table>
-        <!-- 데이터 테이블 -->
-        <tr>
-          <th>No</th>
-          <th>제목</th>
-          <th>상황</th>
-          <th>등록일</th>
-          <th>카테고리</th>
-        </tr>
-        <tr v-for="(item, index) in dataList" :key="index">
-          <td>{{ index + 1 }}</td>
-          <td>{{ item.title }}</td>
-          <td>{{ item.status }}</td>
-          <td>{{ item.date }}</td>
-          <td>{{ item.category }}</td>
-        </tr>
-      </table>
-  
-      <div class="pagination">
-        <!-- 페이지네이션 컨트롤 -->
-        <span @click="changePage(page)" v-for="page in pages" :key="page">{{ page }}</span>
-      </div>
+  <div>
+    <div class="page_btn">
+      <button :class="{ active: isCurrentPage('/') }" @click="navigateTo('/')">데이터 목록</button>
+      <button :class="{ active: isCurrentPage('/user-data-map') }" @click="navigateTo('/user-data-map')">데이터 맵</button>
+      <button :class="{ active: isCurrentPage('/user-history') }" @click="navigateTo('/user-history')">이력 조회</button>
     </div>
-  </template>
-  
+    <div class="search-filter">
+      <input type="date" v-model="startDate" />
+      <input type="date" v-model="endDate" />
+      <input type="text" v-model="searchQuery" placeholder="검색어" />
+      <button @click="fetchData">검색</button>
+    </div>
+    <div v-if="loading">데이터 로딩중...</div>
+    <div v-if="error">{{ error }}</div>
+    <h1>데이터 목록</h1>
+    <button @click="addData">등록</button>
+    <table>
+      <tr>
+        <th>No</th>
+        <th>테이블 물리명</th>
+        <th>테이블 논리명</th>
+        <th>등록일</th>
+      </tr>
+      <tr v-for="(item, index) in filteredData" :key="index">
+        <td>{{ item.no }}</td>
+        <td>{{ item.physicalTableName }}</td>
+        <td>{{ item.logicalTableName }}</td>
+        <td>{{ formatDate(item.registrationDate) }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
 
 <script>
-import './Main.css'
+import axios from 'axios';
+import './Main.css';
+
 export default {
-    name: 'MainPage',
-    components: {
-    },
+  name: 'MainPage',
   data() {
     return {
       startDate: '',
       endDate: '',
-      selectedCategory: '',
       searchQuery: '',
-      dataList: [],
-      pages: [1, 2, 3, 4, 5, 6, 7] // 예시로 페이징 번호를 설정
+      rawData: [], // 서버로부터 받은 원본 데이터
+      loading: false, // 로딩 상태
+      error: null, // 에러 메시지
     };
   },
+  computed: {
+    filteredData() {
+      return this.rawData.filter(item => {
+        const startDateMatch = this.startDate ? new Date(item.registrationDate) >= new Date(this.startDate) : true;
+        const endDateMatch = this.endDate ? new Date(item.registrationDate) <= new Date(this.endDate) : true;
+        const queryMatch = this.searchQuery ? item.logicalTableName.toLowerCase().includes(this.searchQuery.toLowerCase()) || item.physicalTableName.toLowerCase().includes(this.searchQuery.toLowerCase()) : true;
+        return startDateMatch && endDateMatch && queryMatch;
+      });
+    }
+  },
   methods: {
-    navigateToTable() {
-    // '테이블' 버튼 로직
-    //this.$router.push('/table');
-  },
-  navigateToDataMap() {
-    // '데이터맵' 버튼 로직
-    //this.$router.push('/data-map');
-  },
-    searchData() {
-      // 검색 데이터 로직
+    fetchData() {
+      this.loading = true;
+      this.error = null;
+      const token = localStorage.getItem('token');
+      const userEmail = localStorage.getItem('email');
+
+      axios.get(`http://localhost:3001/history/join?email=${userEmail}`, {
+        headers: { 'Authorization': `Bearer ${token}` }
+      })
+      .then(response => {
+        this.rawData = response.data.data;
+      })
+      .catch(error => {
+        this.error = '데이터 로드 중 에러 발생';
+        console.error('Error fetching data:', error);
+      })
+      .finally(() => {
+        this.loading = false;
+      });
+    },
+    formatDate(dateString) {
+      const date = new Date(dateString);
+      return date.toLocaleDateString();
+    },
+    navigateTo(route) {
+      this.$router.push(route);
+    },
+    isCurrentPage(route) {
+      return this.$route.path === route;
     },
     addData() {
-      // 데이터 추가 로직
       this.$router.push('/data-register');
-    },
-    changePage() {
-    //changePage(pageNumber)
-      // 페이지 변경 로직
     }
-    
-  }
+  },
+  mounted() {
+    this.fetchData();
+  },
 };
 </script>
-
