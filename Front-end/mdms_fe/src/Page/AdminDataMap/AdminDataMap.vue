58d286baf792a38cbdfe00f4d6339b3649e9ffe5
Recyclingbottle <hoholeo382@gmail.com>
2023-11-15 16:14:49 +0900

    관리자 - 데이터맵 테스트

--- Front-end/mdms_fe/src/Page/AdminDataMap/AdminDataMap.vue
+++ Front-end/mdms_fe/src/Page/AdminDataMap/AdminDataMap.vue
@@ -16,7 +16,9 @@
     <div v-if="showTableList">
       <h3>테이블 목록</h3>
       <ul>
-        <li v-for="table in tableList" :key="table.no">{{ table.logicalTableName }}</li>
+        <li v-for="table in tableList" :key="table.no" @click="handleTableClick(table.logicalTableName)">
+          {{ table.logicalTableName }}
+        </li>
       </ul>
     </div>
 
@@ -26,11 +28,18 @@
         <li v-for="term in terminologyList" :key="term">{{ term }}</li>
       </ul>
     </div>
+
+    <div ref="chartdiv" style="width: 100%; height: 500px;"></div>
   </div>
 </template>
 
+
 <script>
 import axios from 'axios';
+// amCharts 관련 라이브러리 임포트
+import * as am5 from "@amcharts/amcharts5";
+import * as am5hierarchy from "@amcharts/amcharts5/hierarchy";
+import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
 
 export default {
   name: 'AdminDataMapPage',
@@ -40,13 +49,15 @@ export default {
       tableList: [],
       terminologyList: [],
       showTableList: false,
-      showTerminologyList: false
+      showTerminologyList: false,
+      chart: null // 차트 인스턴스를 저장할 변수
     };
   },
   created() {
-    this.fetchTableData();
+    this.fetchTableData(); // 컴포넌트가 생성될 때 데이터를 가져옵니다.
   },
   methods: {
+    // 서버에서 테이블 데이터를 가져오는 메소드
     async fetchTableData() {
       try {
         const token = localStorage.getItem('token');
@@ -79,9 +90,67 @@ export default {
       this.showTerminologyList = true;
       this.showTableList = false;
     },
+    // 테이블 항목 클릭 시 호출되는 메소드
+    handleTableClick(logicalTableName) {
+      const selectedTable = this.tableData.find(table => table.logicalTableName === logicalTableName);
+      if (selectedTable) {
+        this.createChart(selectedTable);
+      }
+    },
+    // 차트를 생성하는 메소드
+    createChart(tableData) {
+      if (this.chart) {
+        this.chart.dispose(); // 기존 차트가 있다면 제거합니다.
+      }
+
+      let root = am5.Root.new(this.$refs.chartdiv);
+
+      root.setThemes([am5themes_Animated.new(root)]);
+
+      let container = root.container.children.push(am5.Container.new(root, {
+        width: am5.percent(80),
+        height: am5.percent(80),
+        layout: root.verticalLayout
+      }));
+
+      // 차트에 사용할 데이터 구조를 생성합니다.
+      const data = {
+        name: tableData.logicalTableName,
+        children: tableData.stdTerminologyList.map(term => ({
+          name: term,
+          value: tableData.num
+        }))
+      };
+
+      // 차트 생성 및 설정
+      let series = container.children.push(am5hierarchy.ForceDirected.new(root, {
+        singleBranchOnly: false,
+        downDepth: 2,
+        topDepth: 1,
+        initialDepth: 2,
+        valueField: "value",
+        categoryField: "name",
+        childDataField: "children",
+        idField: "value",
+        manyBodyStrength: -15,
+        centerStrength: 0.3,
+        minRadius: am5.percent(6),
+        maxRadius: am5.percent(6)
+      }));
+
+      series.data.setAll([data]);
+
+      this.chart = series; // 차트 인스턴스를 저장합니다.
+    }
+  },
+  beforeDestroy() {
+    if (this.chart) {
+      this.chart.dispose(); // 컴포넌트가 파괴될 때 차트를 정리합니다.
+    }
   }
 };
 </script>
+
 <style scoped>
 body {
   font-family: 'Arial', sans-serif;
